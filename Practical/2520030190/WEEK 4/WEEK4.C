f9c455d588d16e0046f16ea344b0be138898b718
MD AMAN <mdaman@klh.edu.in>
2026-09-15 10:14:19 +0530

    Add C program for process synchronization and zombie handling
    Implement a C program demonstrating process creation and synchronization. Includes handling of zombie processes using wait() and waitpid().

--- Practical/2520030190/WEEK 4/WEEK4.C
+++ Practical/2520030190/WEEK 4/WEEK4.C
@@ -1,3 +1,11 @@
+Write a C program where a parent process creates multiple child processes and synchronizes their completion using wait() and waitpid().
+Compare the behavior of both functions.Create a scenario where a child process becomes a zombie process. Investigate the process table and then 
+modify the program to eliminate zombie processes using proper synchronization techniques.
+
+
+
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
