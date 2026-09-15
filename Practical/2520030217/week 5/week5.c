dc52479042af14af5796a97bd7cf4b7e4fe4d3b5
sreshtapaladugu12 <sreshtapaladugu12@gmail.com>
2026-09-15 09:34:40 +0530

    Update week5.c

--- Practical/2520030217/week 5/week5.c
+++ Practical/2520030217/week 5/week5.c
@@ -1,3 +1,7 @@
+5.Implement a producer-consumer communication system using anonymous pipes where the parent process generates data and the child process consumes it. 
+Measure the communication efficiency.
+Develop a program that executes the equivalent of the shell command: ls -l | grep ".c" using fork(), pipe(), dup2(), and exec() system calls.
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
