d99d78e47242d76734180a56e41f4b2ac8713a67
MD AMAN <mdaman@klh.edu.in>
2026-09-15 10:13:27 +0530

    Implement process creation and state display in C
    Developed a C program that creates a parent and child process using fork(). Added functionality to display PID, PPID, and process states. Included instructions for observing process state transitions using Linux tools.

--- Practical/2520030190/WEEK 3/WEEK3.C
+++ Practical/2520030190/WEEK 3/WEEK3.C
@@ -1,3 +1,10 @@
+Develop a C program using fork() that creates a parent and child process. Display the Process ID (PID), Parent Process ID (PPID), and process states 
+at different stages of execution.Design an experiment to observe process state transitions (Ready, Running, Waiting, Terminated) using Linux monitoring
+tools such as ps, top, and /proc. Document the observations.
+
+
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
